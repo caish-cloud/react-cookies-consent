b28519c4b60f9eaefbdbe2033ddc286ab7bf5e72
tcaish <77754475+tcaish@users.noreply.github.com>
2024-04-23 08:07:42 -0400

    create alert content comp

---
+++ src/components/alert/alert-content.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ThemeStyles } from '../../constants/types';
+import { AlertContainer } from './alert-container';
+
+export type AlertContentProps = {
+  /**
+   * The children of the alert content.
+   */
+  children: React.ReactNode;
+
+  /**
+   * The styles for the container of the content.
+   */
+  containerStyle?: ThemeStyles;
+};
+
+/**
+ * The content of the alert, such as the title and description.
+ * @param props - The properties to pass to the component.
+ */
+export function AlertContent(props: AlertContentProps) {
+  return (
+    <AlertContainer
+      defaultStyle={defaultContainerStyle}
+      flexDirection="column"
+      userDefinedStyle={props.containerStyle}
+    >
+      {props.children}
+    </AlertContainer>
+  );
+}
+
+const defaultContainerStyle: ThemeStyles = { dark: {}, light: {} };
