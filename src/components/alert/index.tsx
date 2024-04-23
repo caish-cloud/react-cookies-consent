353c8c174710d950c0e5dada267e96f02aa60a65
tcaish <77754475+tcaish@users.noreply.github.com>
2024-04-23 08:07:57 -0400

    use dot notation for nested comps

--- src/components/alert/index.tsx
+++ src/components/alert/index.tsx
@@ -1,8 +1,23 @@
-export { AlertRoot as CookiesConsentAlert } from './alert-root';
-export type { AlertRootProps as CookiesConsentAlertProps } from './alert-root';
+// export { AlertRoot as CookiesConsentAlert } from './alert-root';
+import { AlertContent } from './alert-content';
+import { AlertDescription } from './alert-description';
+import { AlertRoot } from './alert-root';
+import { AlertTitle } from './alert-title';
 
-export { AlertTitle as CookiesConsentAlertTitle } from './alert-title';
+export type { AlertContentProps as CookiesConsentAlertContentProps } from './alert-content';
+export type { AlertDescription as CookiesConsentAlertDescriptionProps } from './alert-description';
+export type { AlertRootProps as CookiesConsentAlertProps } from './alert-root';
 export type { AlertTitleProps as CookiesConsentAlertTitleProps } from './alert-title';
 
-export { AlertDescription as CookiesConsentAlertDescription } from './alert-description';
-export type { AlertDescriptionProps as CookiesConsentAlertDescriptionProps } from './alert-description';
+const CookiesConsentAlertContent = AlertContent;
+const CookiesConsentAlertDescription = AlertDescription;
+const CookiesConsentAlertTitle = AlertTitle;
+
+export const CookiesConsentAlert = AlertRoot as typeof AlertRoot & {
+  Content: typeof CookiesConsentAlertContent;
+  Description: typeof CookiesConsentAlertDescription;
+  Title: typeof CookiesConsentAlertTitle;
+};
+CookiesConsentAlert.Content = CookiesConsentAlertContent;
+CookiesConsentAlert.Description = CookiesConsentAlertDescription;
+CookiesConsentAlert.Title = CookiesConsentAlertTitle;
