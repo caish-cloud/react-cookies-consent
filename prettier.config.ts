3bc0047cec00f0d2697f17aa65e588729524990f
Caish Cloud Admin <134313463+caish-cloud-admin@users.noreply.github.com>
2024-08-13 14:43:42 -0400

    update prettier file

---
+++ prettier.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  fluid: false,
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+};
